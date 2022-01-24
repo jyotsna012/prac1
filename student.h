537be0cc2008f6eecafb0e5d984a1a4b9d2a3f63
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-23 18:35:42 -0800

    Update student.h

--- student.h
+++ student.h
@@ -1,1 +1,11 @@
-
+#include <cstring>
+#include <string.h>
+#pragma once
+using namespace std;
+class Student{
+  public:
+  Student();
+  int age = new int;
+  char *name() = new char[80];
+  float gpa = new float;
+}
