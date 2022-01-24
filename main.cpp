0e20f9c5b7f138d919593e9901527bfc059099a2
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-23 18:19:52 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+
+cout << "how many numbers do you want to enter" << endl;
+int size;
+cin >> size;
+int *array = new array[size];
+
+}
