72a816bae38d376737cc9007092de4cd1d5279ee
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-23 19:00:29 -0800

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include "Student.h"
+#include <cstring>
 
+using namespace std;
+
+Student :: Student(){
+
+}
+
+void Student :: setStudentDetails(char* nName,int nAge,float nGpa){
+  strcpy(name, nName);
+  *age = nAge;
+  *gpa = nGpa;  
+}
+
+int* Student :: getAge(){
+  return age;
+}
+
+char* Student :: getName(){
+  return name;
+
+}
+
+float* Student :: getGpa(){
+  return gpa;
+
+}
