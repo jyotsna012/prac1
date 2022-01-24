05d44545793070399c334733a4094383f0275c5c
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-01-23 18:27:35 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,4 +10,16 @@ int size;
 cin >> size;
 int *array = new int[size];
 
+cout << "Enter " << size << " items" << endl;
+for(int i = 0; i < size; i++){
+  cin >> array[i]
+}
+
+cout << "You have entered: "  << endl;
+
+for(int i = 0; i < size; i++){
+  cout << array[i] << endl;
+}
+
+
 }
